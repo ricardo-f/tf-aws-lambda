9090ebda5e4f9b2fc3e3f83c1a1ca8e19f1a329a
ricardo-f <riclima1990@gmail.com>
2022-11-01 21:32:58 -0300

    yeap doing a aws lambda calculator...

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # tf-aws-lambda
 
 _Work in progress..._
+
+ curl -X POST \
+ 'yourUrlNotMine' \
+  -H 'Content-Type: application/json' \
+  -d '{"num1": 10, "ops": "sum", "num2": 10 }'
+
+ curl -X POST \
+ 'yourUrlNotMine' \
+  -H 'Content-Type: application/json' \
+  -d '{"num1": 10, "ops": "subtraction", "num2": 10 }'
+
+ curl -X POST \
+ 'yourUrlNotMine' \
+  -H 'Content-Type: application/json' \
+  -d '{"num1": 10, "ops": "multiplication", "num2": 5 }'
+
+curl -X POST \
+ 'yourUrlNotMine' \
+  -H 'Content-Type: application/json' \
+  -d '{"num1": 10, "ops": "division", "num2": 2 }'
+
+curl -X POST \
+ 'yourUrlNotMine' \
+  -H 'Content-Type: application/json' \
+  -d '{"num1": 10, "ops": "division", "num2": 0 }'
--- src/index.js
+++ src/index.js
@@ -1,9 +1,53 @@
-exports.handler = async  (event) => {
+exports.handler = async (event) => {
   let body = JSON.parse(event.body)
-  const product = body.num1 * body.num2;
-  const response = {
-      statusCode: 200,
-      body: "The product of " + body.num1 + " and " + body.num2 + " is " + product,
-  };
-  return response;
+  const num1 = body.num1
+  const num2 = body.num2
+  const ops = body.ops
+  const result = await lambdaCalc(num1, ops, num2)
+  return result;
 };
+
+function lambdaCalc(num1, ops, num2) {
+  if (ops === "sum") {
+    const result = num1 + num2
+    let response = {
+      statusCode: 200,
+      body: `the total of the ${ops} is ${result}`,
+    }
+    return response;
+  }
+  
+  if (ops === "subtraction") {
+    const result = num1 - num2
+    let response = {
+      statusCode: 200,
+      body: `the total of the ${ops} is ${result}`,
+    }
+    return response;
+  }
+
+  if (ops === "multiplication") {
+    const result = num1 * num2
+    let response = {
+      statusCode: 200,
+      body: `the total of the ${ops} is ${result}`,
+    }
+    return response;
+  }
+
+  if (ops === "division") {
+    if ( num2 === 0 ) {
+      let response = {
+        statusCode: 400,
+        body: "num2 can't be 0",
+      };
+      return response;
+    }
+    const result = num1 / num2
+    let response = {
+      statusCode: 200,
+      body: `the total of the ${ops} is ${result}`,
+    }
+    return response;
+  }
+}
